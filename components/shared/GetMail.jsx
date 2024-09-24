1f10c0ed0507a35fb9a5baf0fb9f9649b3986da5
raji <kingraj1344@gmail.com>
2024-09-24 08:52:15 +0100

    email form has been added to a database

--- components/shared/GetMail.jsx
+++ components/shared/GetMail.jsx
@@ -1,15 +1,24 @@
 import { Mail } from 'lucide-react'
 import React from 'react'
+import { handleSubmit } from '@/lib/formActions'
+import { useToast } from '@chakra-ui/react';
+
 
 const GetMail = () => {
+  const toast = useToast();
+
   return (
-    <form action="" className='relative size-fit mt-2'>
-        <input type="email" className='bg-white rounded-3xl h-[45px] outline-none border-none placeholder:text-bold placeholder:font-medium 
+    <form action={async (formData) => {
+      console.log('running')
+      await handleSubmit(formData,toast)
+     
+    }} className='relative size-fit mt-2'>
+      <input type="email" name='email' className='bg-white rounded-3xl h-[45px] outline-none border-none placeholder:text-bold placeholder:font-medium 
         placeholder:text-[12px] text-[12px] pl-[50px] pr-[120px] flex items-center w-[350px]' placeholder='Enter your Email' />
-        <Mail className='text-bold size-4 absolute top-[15px] left-6' />
-        <button className='bg-bold text-white font-medium flex items-center justify-center text-[12px] h-[80%] absolute top-1 right-2 px-5 rounded-3xl cursor-pointer'>
-          Subscribe</button>
-    </form> 
+      <Mail className='text-bold size-4 absolute top-[15px] left-6' />
+      <button className='bg-bold text-white font-medium flex items-center justify-center text-[12px] h-[80%] absolute top-1 right-2 px-5 rounded-3xl cursor-pointer'>
+        Subscribe</button>
+    </form>
   )
 }
 
