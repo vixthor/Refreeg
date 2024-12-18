d88b2a92378fd776b4cd7f3a60b3347c10399f20
Nomso <nomsounaogu@gmail.com>
2024-12-18 23:53:19 +0100

    feat: integrate Paystack donation button with form inputs
    - Added Paystack donation button to the Donate component
    - Implemented form for collecting user details (email, amount, name, phone)
    - Integrated Paystack public key from environment variable
    - Styled input fields and button using Tailwind CSS
    - Added success and close alerts after donation

--- app/(root)/(home)/components/CTA.jsx
+++ app/(root)/(home)/components/CTA.jsx
@@ -1,5 +1,5 @@
-import Button from '@/components/shared/Button'
-import { socialLinks } from '@/constants'
+import Button from "../../../../components/shared/Button";
+import { socialLinks } from "../../../../constants";
 import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
