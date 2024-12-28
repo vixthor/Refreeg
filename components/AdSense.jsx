9c309f115e80cf38e05b9d765de7dfc5dd7c26e5
raji <kingraj1344@gmail.com>
2024-12-28 12:40:00 +0100

    add AdSense and AdBanner components for ad integration

---
+++ components/AdSense.jsx
@@ -0,0 +1,17 @@
+import Script from 'next/script';
+import React from 'react'
+
+
+
+const AdSense = ({ pId }) => {
+  return (
+    <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+        crossOrigin='anonymous'
+        strategy='afterInteractive'
+    />
+  )
+}
+
+export default AdSense
